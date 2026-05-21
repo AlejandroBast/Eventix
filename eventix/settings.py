9022b5a230fe680827214f5b2538f58d8647cce3
AlejandroBast <nicohlas.personal@gmail.com>
2026-05-21 04:14:03 -0500

    config para host

--- eventix/settings.py
+++ eventix/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '1') == '1'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'aleybast.pythonanywhere.com' ,'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'aleybast.pythonanywhere.com').split(',')
 
 
 # Application definition
